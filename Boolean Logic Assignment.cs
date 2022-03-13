44ea3164f27189ab9c1b313301f9b448f8f8ef1e
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2022-03-13 01:28:24 -0400

    2nd commit

--- Boolean Logic Assignment.cs
+++ Boolean Logic Assignment.cs
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int main() {
-  int age, speedingTickets;
-  string DUI;
-  cout<<" What is your age? "<<endl;
-  cin>>age;
-  cout<<" Have you ever had a DUI? "<<endl;
-  cin>>DUI;
-  cout<<" How many speeding tickets do you have? "<<endl;
-  cin>>speedingTickets;
-  bool ans;
-  if(age>15 && DUI=="false" && speedingTickets<=3){
-    cout<<"Qualified ?"<<endl;
-    cout<<"true"<<endl;
-    ans = true;
-  }
-  else{
-    cout<<"Qualified ?"<<endl;
-    cout<<"false"<<endl;
-    ans = false;
-  }
-  return ans; // if exit status is 1 then true
+   float weight, width, height, length, quote;
+   Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\n");
+   Console.WriteLine("Please enter the package weight:\n");
+   scanf("%f", &weight);
+   if(weight > 50) {
+       Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+       return 0;
+   }
+   Console.WriteLine("Please enter the package width:\n");
+   scanf("%f", &width);
+   Console.WriteLine("Please enter the package height:\n");
+   scanf("%f", &height);
+   Console.WriteLine("Please enter the package length:\n");
+   scanf("%f", &length);
+   if(width+height+length > 50) {
+       Console.WriteLine("Package too big to be shipped via Package Express.");
+       return 0;
+   }
+   quote = width*height*length*weight/100;
+   Console.WriteLine("Your estimated total for shipping this package is: $%.2f\n Thank you.", quote);
+   return 0;
 }
